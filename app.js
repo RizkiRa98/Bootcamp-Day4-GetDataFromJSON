69072b07d5c6a5a5a08e50b0e66a766738cad5d3
RizkiRa98 <rizkiramadhan350@gmail.com>
2023-02-09 11:10:03 +0700

    Challenge Express Passing Array Ke Page Contact

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
+const express = require("express"); //modul express
+const app = express(); //function express
+const port = 3000;
+
+const contact = [
+  {
+    name: "Muhamad Rizki Ramadhan",
+    email: "rizkiramadhan350@gmail.com",
+  },
+  {
+    name: "Iqbal",
+    email: "iqbal@gmail.com",
+  },
+  {
+    name: "Taufik",
+    email: "Taufik@gmail.com",
+  },
+];
+
+//set EJS
+app.set("view engine", "ejs");
+
+// '/' meaning root
+app.get("/", (req, res) => {
+  // res.send('Hello World!')
+  res.render("index", {
+    root: __dirname,
+  });
+});
+app.get("/about", (req, res) => {
+  // res.send('<h1> Halaman About </h1>');
+  res.render("about", {
+    root: __dirname,
+  });
+});
+
+app.get("/contact", (req, res) => {
+  // res.send('<h1> Halaman contact </h1>');
+  res.render("contact", {
+    contact,
+  });
+});
+
+//memanggil halaman menggunakan roote parameter
+app.get("/product/:productID/category/:categoryID", (req, res) => {
+  // res.send('<h1> Halaman contact </h1>');
+  res.send(req.params);
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("Page Not Foung: 404");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 // const http = require('http');
 // const fs = require('fs');
 
@@ -28,42 +86,3 @@
 // }).listen(3000, () => {
 //   console.log('Server is listening on port 3000');
 // });
-const express = require('express') //modul express
-const app = express() //function express
-const port = 3000
-
-// '/' meaning root
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.sendFile('./index.html', {
-    root: __dirname
-  })
-})
-app.get('/about', (req, res) => {
-  // res.send('<h1> Halaman About </h1>');
-  res.sendFile('./about.html', {
-    root: __dirname
-  })
-})
-
-app.get('/contact', (req, res) => {
-  // res.send('<h1> Halaman contact </h1>');
-  res.sendFile('./contact.html', {
-    root: __dirname
-  })
-})
-
-//memanggil halaman menggunakan roote parameter
-app.get('/product/:productID/category/:categoryID', (req, res) => {
-  // res.send('<h1> Halaman contact </h1>');
-  res.send(req.params)
-})
-
-app.use('/', (req, res) => {
-  res.status(404);
-  res.send('Page Not Foung: 404')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
